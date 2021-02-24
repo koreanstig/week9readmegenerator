04ad533d3afba1a4e3d755b1d56a802d582fdc3b
Jung Lee <koreanstig@gmail.com>
2021-02-24 11:06:58 -0800

    redid my own readme file so I could use that as base code for the readme generator, also created the prompt questions

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# week9readmegenerator
+<h1 align="center">Week9ReadMeGenerator</h1>
+
+## Description
 Using the inquirer npm, I will create an application that will create a README file using the command line
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributors](#contributors)
+- [Questions](#questions)
+
+## Installation
+- Clone this repo
+- Download the Inquirer NPM (npm i inquierer)
+- Answer the questions in the terminal
+
+## Usage
+Use this to create a quick and easy README file for whatever new project you're working on!
+
+## Contributors
+Jung (David) Lee
+
+## Questions
+My Github: [koreanstig](https://github.com/koreanstig)<br />
+<br />
+My email: jungdavidlee@gmail.com<br />
--- index.js
+++ index.js
@@ -1,2 +1,65 @@
+// initialized my npm's
 const inquirer = require('inquirer');
 const fs = require('fs');
+// started by creating the prompts needed via the inquirer npm
+//Prompt the user questions to populate the README.md
+inquirer.prompt([{
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Write a brief description of your project: "
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What is the installation process: ",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What will your project be used for?"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Chose the appropriate license for this project: ",
+        choices: [
+            "Academic",
+            "Apache",
+            "GNU",
+            "ISC",
+            "MIT",
+            "Mozilla",
+            "Open"
+        ]
+    },
+    {
+        type: "input",
+        name: "contributors",
+        message: "Who has contributed to this project?"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "How do I test your project?"
+    },
+    {
+        type: "input",
+        name: "questions",
+        message: "What do I do if I have any questions? "
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "What is your GitHub username? "
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email? "
+    }
+])
